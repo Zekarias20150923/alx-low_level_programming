bcbac044f0c2ec9350aeb684fb9fafe2e30683d7
Zeelshaddai Alemnew <zeelecaa2011@gmail.com>
2023-07-13 12:20:31 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main- Entry point
- * Retur: always 0 (success)
+ * Retur: always 1 (success)
  */
 int main(void)
 {
-       write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+       write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
         return (1);
 }
