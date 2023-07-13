21b7d459cb4c07c3bb46c2e8675386651055cf4e
Zeelshaddai Alemnew <zeelecaa2011@gmail.com>
2023-07-13 10:29:18 -0700

    c introduction tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Retur: always 0 (success)
+ */
+int main(void)
+{
+	printf("size of a char: %d bytes(s)\n", sizeof(char));
+        printf("size of an int: %d bytes(s)\n", sizeof(int));
+        printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+        printf("size of a long long: %d bytes(s)\n", sizeof(long long));
+        printf("size of a float: %d bytes(s)\n", sizeof(float));
+        return (0);
+}
